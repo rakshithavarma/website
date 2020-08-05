4589644be6b315e6de81ae0bcaad92e6ede38b5d
Sreekar <tankalasrkr@gmail.com>
2020-08-05 22:37:06 +0530

    sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,18 @@ $(window).scroll(function() {
 
 });
 
+$("[data-trigger]").on("click", function () {
+	var trigger_id = $(this).attr('data-trigger');
+	$(trigger_id).toggleClass("show");
+	$('body').toggleClass("offcanvas-active");
+});
+
+// close button 
+$(".btn-close").click(function (e) {
+	$(".navbar-collapse").removeClass("show");
+	$("body").removeClass("offcanvas-active");
+});
+
 // $(document).ready(function () {
 //     $(document).click(function (event) {
 //         var clickover = $(event.target);
