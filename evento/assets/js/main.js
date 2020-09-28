4b8fced8165f5c10fa67e542ff67a170d77dae17
Sreekar <tankalasrkr@gmail.com>
2020-09-28 16:49:10 +0530

    event-timer-matter

--- evento/assets/js/main.js
+++ evento/assets/js/main.js
@@ -220,7 +220,7 @@
     
 
     // Use this for real timer date
-    /*  var timerdate = "2020/01/01"; */
+      var timerdate = "2020/10/12"; 
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span><p>Days</p> </div>" + "<div class='cd-item'><span>%H</span><p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span><p>Min</p> </div>" + "<div class='cd-item'><span>%S</span><p>Sec</p> </div>"));
